751596cc8165f82b7de84e7834850870ce323521
Spencer Smith <spencersmith24@outlook.com>
2024-11-26 17:10:23 -0500

    re-arrange tables

--- galaxy-gas/Scripts/table.gd
+++ galaxy-gas/Scripts/table.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 var is_upgraded = false
 
+@onready var small_chairs = $Small/Chairs.get_children()
+@onready var big_chairs = $Large/topChairs.get_children() + ($Large/botChairs.get_children())
+
 func upgrade_table():
 	$Small.process_mode = Node.PROCESS_MODE_DISABLED
 	$Small.visible = false
@@ -10,3 +13,15 @@ func upgrade_table():
 	$Large.visible = true
 	is_upgraded = true
 	$"../..".max_customers += 2
+	
+func is_full():
+	if not is_upgraded:
+		for chair in small_chairs:
+			if not chair.is_taken:
+				return false
+		return true
+	else:
+		for chair in big_chairs:
+			if not chair.is_taken:
+				return false
+		return true
