b41d13550147984f64e1ab2e8cf8da56eda8f87d
Spencer Smith <spencersmith24@outlook.com>
2024-11-26 19:11:13 -0500

    fix table upgrading
    re-assign customers to a table if theirs is upgraded

--- galaxy-gas/Scripts/GameController.gd
+++ galaxy-gas/Scripts/GameController.gd
@@ -5,7 +5,8 @@ var money_amt
 
 # Local globals
 var rng = RandomNumberGenerator.new()
-const customer = preload("res://Scenes/Scenes/customer_reg_2d.tscn")
+
+var customers = ["res://Scenes/Game Objects/Characters/customer_lumberjack.tscn","res://Scenes/Game Objects/Characters/customer_oldguy.tscn","res://Scenes/Game Objects/Characters/customer_pinkgirl.tscn"]
 
 @export var max_customers: int = 8
 @export var inn_capacity = 4
@@ -17,14 +18,14 @@ const customer = preload("res://Scenes/Scenes/customer_reg_2d.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-		pass
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	money_amt = Globals.money_amt
 	MONEY_LABEL.text = str(money_amt)
 	
-	if not are_tables_full():
+	if not are_tables_full() and not are_tables_upgrading():
 		if $CustomerSpawnTimer.is_stopped():
 			$CustomerSpawnTimer.start()
 		
@@ -37,7 +38,7 @@ func _on_customer_spawn_timer_timeout() -> void:
 	spawn_new_customer()
 	
 func spawn_new_customer():
-	var new_customer = customer.instantiate()
+	var new_customer = load(customers[randi_range(0, customers.size() - 1)]).instantiate()
 	if Globals.customers_staying.size() < inn_capacity:
 		var random_value = randf() * 100
 		if random_value < stay_chance:
@@ -46,6 +47,7 @@ func spawn_new_customer():
 			pass
 	else:
 		Globals.customers_in_store.append(new_customer)
+	new_customer.global_position = Vector2(0, 500)
 	$Customers.add_child(new_customer)
 
 
@@ -60,3 +62,12 @@ func are_tables_full():
 		if not table.is_full():
 			return false
 	return true
+
+func are_tables_upgrading():
+	for table in $Tables.get_children():
+		if table.is_upgrading:
+			return true
+	return false
+
+func _get_customer():
+	return customers[randi_range(0, customers.size() - 1)]
