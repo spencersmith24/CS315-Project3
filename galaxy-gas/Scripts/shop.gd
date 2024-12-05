16863f1774ee26e59b3894ee4d102c7c245b7a20
Spencer Smith <spencersmith24@outlook.com>
2024-12-05 12:56:06 -0500

    update new stats node
    newly added stats node displays the current number of seats and beds available

--- galaxy-gas/Scripts/shop.gd
+++ galaxy-gas/Scripts/shop.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var tables = $"../../../Tables".get_children()
 @onready var rooms = $"../../../Rooms".get_children()
+@onready var root_node = $"../../.."
 
 @export var table_upgrade_cost = 100
 @export var table_upgrade_cost_multiplier = 1.8
@@ -44,12 +45,13 @@ func _on_table_button_pressed() -> void:
 		Globals.money_amt -= table_upgrade_cost
 		table_upgrade_cost *= table_upgrade_cost_multiplier
 		table_upgrade_cost = int(table_upgrade_cost + .5)
-		$"Upgrades/TablesButton/Tables/Label".text = "$" + str(table_upgrade_cost)
+		$"Upgrades/TablesButton/Tables/Price".text = "$" + str(table_upgrade_cost)
 		for table in tables:
 			if not table.is_upgraded:
 				table.upgrade_table()
 				tables_upgraded += 1
 				break
+		$Stats/Chairs.text = "Chairs: " + str(root_node.max_customers)
 	if tables_upgraded >= tables.size():
 		$"Upgrades/TablesButton/Tables".disabled = true
 	pass
@@ -60,14 +62,15 @@ func _on_inn_capacity_pressed() -> void:
 		Globals.money_amt -= inn_capacity_upgrade_cost
 		inn_capacity_upgrade_cost *= inn_capacity_upgrade_cost_multiplier
 		inn_capacity_upgrade_cost = int(inn_capacity_upgrade_cost + .5)
-		$"Upgrades/InnCapacity/inn_capacity/Label".text = "$" + str(inn_capacity_upgrade_cost)
+		$"Upgrades/RoomsButton/Rooms/Price".text = "$" + str(inn_capacity_upgrade_cost)
 		for room in rooms:
 			if not room.is_upgraded:
 				room.upgrade_room()
 				rooms_upgraded += 1
 				break
+		$Stats/BedSpace.text = "Bed Space: " + str(root_node.inn_capacity)
 	if rooms_upgraded >= rooms.size():
-		$"Upgrades/InnCapacity/inn_capacity".disabled = true
+		$"Upgrades/RoomsButton/Rooms".disabled = true
 		
 		# TODO CREATE A NEW ROOM
 	pass # Replace with function body.
