1578733ab4e74f008c035358e67f3da4c401578b
Evan Greenfield <evangreenfield@live.com>
2024-12-08 16:07:44 -0500

    make arcade machine show click value

--- galaxy-gas/Scripts/arcade_machine.gd
+++ galaxy-gas/Scripts/arcade_machine.gd
@@ -1,9 +1,22 @@
 extends Area2D
 
+@export var label: Resource
+
+@export var click_value = 1
+
 func _on_input_event(_viewport, event, _shape_idx):
+	
 	if event is InputEventMouseButton:
 		if Input.is_action_just_pressed("click"):
 			$AnimationPlayer.play("RESET")
 			$AnimationPlayer.play("jiggle")
 			
-			Globals.root_node.money_amt += 1
+			Globals.root_node.money_amt += click_value
+			
+			var new_label = label.instantiate()
+			new_label.position = self.global_position
+			new_label.get_node("Label").text = "$" + str(click_value)
+			get_parent().add_child(new_label)
+			
+			await get_tree().create_timer(6).timeout
+			new_label.queue_free()
